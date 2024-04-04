0f933fc6b1c259fa6434b69e6df69f809285f86b
Ana Teixeira <acrispteixeira@gmail.com>
2024-04-03 21:45:32 -0300

    Módulo 5 - Flyway

---
+++ FrameBlog/src/main/java/com/descomplica/FrameBlog/db/migration/V2__Create_user.sql
@@ -0,0 +1,14 @@
+INSERT INTO frameBlog.user
+(user_id,
+email,
+name,
+password,
+role,
+username)
+VALUES
+(1,
+"flyway@test.com",
+"Flyway Version 2",
+"$2a$10$jhLDsjRjrQsyvQBkCXsklu867hBTzT6VqSiBAMXUbn1UfICS6eklT",
+0,
+"flyway");
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Framework
+
+Documentação Módulo 5:
+https://dev.mysql.com/downloads/workbench/
+https://dev.mysql.com/downloads/mysql/
+
+A senha do My Sql é determinada na configuração!

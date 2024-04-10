ed4e8e717d0d738f95eab51cf417b629a1f8eee5
Ana Teixeira <acrispteixeira@gmail.com>
2024-04-09 21:05:00 -0300

    Módulo 13 - Service Discovery

---
+++ FrameBlog/src/main/java/com/descomplica/FrameBlog/controllers/ServiceInstanceRestController.java
@@ -0,0 +1,23 @@
+package com.descomplica.FrameBlog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/service-instances")
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("${spring.application.name}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
